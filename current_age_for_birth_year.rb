b6c15ca1a83646ae0de15ee1acaa940303a1fbee
Tom Storm <tstorm@invoca.com>
2017-12-28 22:08:09 +0000

    automatically backed up by learn

--- current_age_for_birth_year.rb
+++ current_age_for_birth_year.rb
@@ -1,6 +1,6 @@
 
 def current_age_for_birth_year(birth_year)
-  current_year = Time.new.year  
+  current_year = Time.new.year
   age = current_year - birth_year
-  puts "Your age is #{ age }"  
+  puts "Your age is #{ age }"
 end  
